b5b2f4e4cee7ed08c135685f9909db0b75984d88
Kae Petrin <kmaepetrin@gmail.com>
2022-12-22 12:30:00 -0800

    adds mobile jumplink styles, fixes date rendering

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,4 @@
 require("./copy-box");
+var $ = require("./lib/qsa");
+
+$(".todays-date").forEach(el => el.innerHTML = new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'long', day: '2-digit'}).format(new Date()))
